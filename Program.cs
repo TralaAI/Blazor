91b896946ecee379194ca03d145186e02f412f44
MathijsH6 <mathijs.haest@ziggo.nl>
2025-06-09 12:13:41 +0200

    feat: Add AI predictions page and enhance home page layout

--- Program.cs
+++ Program.cs
@@ -1,11 +1,9 @@
-using Blazor.Services;
 using Blazor.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents().AddInteractiveServerComponents();
-builder.Services.AddTransient<IExampleService, ExampleService>();
 
 var app = builder.Build();
 
@@ -22,7 +20,6 @@ app.UseHttpsRedirection();
 app.UseAntiforgery();
 
 app.MapStaticAssets();
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
 
 app.Run();
