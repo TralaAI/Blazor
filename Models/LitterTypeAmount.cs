1e3af55bb2be9a54be8b84480984964edf4ec147
SAAB.W <woutersaab@gmail.com>
2025-06-19 12:52:48 +0200

    Add LitterTypeAmount model to represent various litter types and their amounts

---
+++ Models/LitterTypeAmount.cs
@@ -0,0 +1,10 @@
+namespace Blazor.Models {
+    public class LitterTypeAmount
+    {
+        public int? Organic { get; set; }
+        public int? Paper { get; set; }
+        public int? Plastic { get; set; }
+        public int? Glass { get; set; }
+        public int? Metal { get; set; }
+    }
+}
